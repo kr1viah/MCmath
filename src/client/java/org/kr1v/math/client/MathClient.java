9f300a6d304d06e53c70b28159376d47db42725f
kr1viah_ <kravpolet@gmail.com>
2025-07-06 11:46:02 +0200

    Initial commit

---
+++ src/client/java/org/kr1v/math/client/MathClient.java
@@ -0,0 +1,10 @@
+package org.kr1v.math.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MathClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/kr1v/math/Math.java
@@ -0,0 +1,10 @@
+package org.kr1v.math;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Math implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
